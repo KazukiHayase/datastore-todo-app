718d48d92683df676c74535bdd18b771dea2a048
KazukiHayase <k.hayase@buysell-technologies.com>
2022-08-10 20:12:14 +0900

    add adding todo

--- graph/resolver/schema.resolvers.go
+++ graph/resolver/schema.resolvers.go
@@ -6,14 +6,50 @@ package resolver
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"time"
 
+	"cloud.google.com/go/datastore"
 	"github.com/KazukiHayase/datastore-todo-app/graph/generated"
 	"github.com/KazukiHayase/datastore-todo-app/graph/model"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	dsClient, err := datastore.NewClient(ctx, r.Config.GCP.ProjectID)
+	if err != nil {
+		return nil, err
+	}
+	defer dsClient.Close()
+
+	// 本当はgqlgenのmodelと共通で使用するようにしたい
+	type Todo struct {
+		id        int64
+		Text      string    `datastore:"text"`
+		Done      bool      `datastore:"done"`
+		CreatedAt time.Time `datastore:"createdAt"`
+	}
+
+	key := datastore.IncompleteKey("Todo", nil)
+	newKey, err := dsClient.Put(ctx, key, &Todo{
+		Text:      input.Text,
+		Done:      false,
+		CreatedAt: time.Now(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Todo{
+		ID:   strconv.Itoa(int(newKey.ID)),
+		Text: input.Text,
+		Done: false,
+		// TODO: 仮
+		User: &model.User{
+			ID:   "UserID",
+			Name: "Name",
+		},
+	}, nil
 }
 
 // Todos is the resolver for the todos field.
